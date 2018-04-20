f8569b9e2a606756bd17eb82f9b7d8a01f17988d
Gabriel de Oliveira Ferreira <gabrieloferreira.u4@gmail.com>
2018-04-20 09:10:30 -0300

    mudanças cedup
    codigo finalizado

--- ExercicioEstrutura10.java
+++ ExercicioEstrutura10.java
@@ -9,6 +9,13 @@ public class ExercicioEstrutura10{
         double base1 = Double.parseDouble(JOptionPane.showInputDialog("Base 1"));
         double base2 = Double.parseDouble(JOptionPane.showInputDialog("Base 2"));
 
-        double 
+        double alturaconta = altura1-altura2;
+        double baseconta = base1-base2;
+
+        if(alturaconta==0 && baseconta==0){
+            JOptionPane.showMessageDialog(null, "Isso é um retangulo!");
+        }else{
+            JOptionPane.showMessageDialog(null, "Isso não é um retangulo!");
+        }
     }
 }
